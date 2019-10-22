18eff30e3a36a547a7bc3ef7e4021f24ec53c0fa
Bricx Carasco <bricxcarasco.gss@gmail.com>
2019-10-22 11:53:56 +0800

    Display all questions with pagination

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| 
+| php artisan vendor:publish --tag=laravel-pagination
 |
 */
 
@@ -18,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('questions', 'QuestionsController');
