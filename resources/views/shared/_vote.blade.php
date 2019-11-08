ba185a5a055a8e93192c0b4eb2c92f2ff02e8e90
Bricx Carasco <bricxcarasco.gss@gmail.com>
2019-11-08 11:57:10 +0800

    Implement DRY principle on the application views

---
+++ resources/views/shared/_vote.blade.php
@@ -0,0 +1,54 @@
+@if ($model instanceof App\Question)
+    @php
+        $name = 'question';
+        $firstURISegment = 'questions';
+    @endphp
+@elseif ($model instanceof App\Answer)
+    @php
+        $name = 'answer';
+        $firstURISegment = 'answers';
+    @endphp
+@endif
+
+@php
+    $formId = $name . "-" . $model->id;
+    $formAction = "/{$firstURISegment}/{$model->id}/vote";
+@endphp
+<div class="d-flex flex-column vote-controls">
+    {{-- VOTE UP --}}
+    <a href="#" title="This {{ $name }} is useful" 
+        class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+        onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();"
+        >
+        <i class="fas fa-caret-up fa-3x"></i>
+    </a>
+    <form action="{{ $formAction }}" method="POST" id="up-vote-{{ $formId }}" style="display:none;">
+        @csrf
+        <input type="hidden" name="vote" value="1">
+    </form>
+
+    {{-- VOTE COUNT --}}
+    <span class="votes-count">{{ $model->vote_count }}</span>
+
+    {{-- VOTE DOWN --}}
+    <a href="#" title="This {{ $name }} is not useful" 
+        class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+        onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();"
+        >
+        <i class="fas fa-caret-down fa-3x"></i>
+    </a>
+    <form action="{{ $formAction }}" method="POST" id="down-vote-{{ $formId }}" style="display:none;">
+        @csrf
+        <input type="hidden" name="vote" value="-1">
+    </form>
+
+    @if ($model instanceof App\Question)
+        @include('shared._favorite', [
+            'model' => $model
+        ])
+    @elseif ($model instanceof App\Answer)
+        @include('shared._accept', [
+            'model' => $model
+        ])
+    @endif
+</div>
