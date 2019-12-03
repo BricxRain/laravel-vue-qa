58df477fd9e09ce0b93e8b7c22178a1ed2fe3cc4
Bricx Carasco <bricxcarasco.gss@gmail.com>
2019-12-03 16:37:43 +0800

    Create Pagination component and setup it

---
+++ resources/assets/js/components/Questions.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="card-body">
+            <div v-if="questions.length > 0">
+                <question-excerpt v-for="question in questions" :question="question" :key="question.id"></question-excerpt>
+            </div>
+            
+            <div v-else class="alert alert-warning">
+                {{ questions.length }}
+                <strong>Sorry</strong> There are no questions available.
+            </div>
+
+            <!-- pagination goes here -->
+        </div>
+        <div class="card-footer">
+            <pagination :meta="meta" :links="links"></pagination>
+        </div>
+    </div>
+</template>
+<script>
+import QuestionExcerpt from '../components/QuestionExcerpt.vue';
+import Pagination from '../components/Pagination.vue';
+
+export default {
+
+    components: {
+        QuestionExcerpt,
+        Pagination
+    },
+
+    data () {
+        return {
+            questions: [],
+            meta: {},
+            links: {}
+        }    
+    },
+
+    mounted () {
+        this.fetchQuestions();
+    },
+
+    watch: {
+        "$route": 'fetchQuestions'
+    },
+
+    methods: {
+        fetchQuestions () {
+            axios.get('/questions', { params: this.$route.query })
+                .then(( {data} ) => {
+                    this.questions = data.data;
+                    this.meta = data.meta;
+                    this.links = data.links;
+                });
+        }
+    }
+}
+
+</script>
--- resources/assets/js/pages/QuestionsPage.vue
+++ resources/assets/js/pages/QuestionsPage.vue
@@ -12,50 +12,22 @@
                         </div>
                     </div>
 
-                    <div class="card-body">
-                        <div v-if="questions.length > 0">
-                            <question-excerpt v-for="question in questions" :question="question" :key="question.id"></question-excerpt>
-                        </div>
-                        
-                        <div v-else class="alert alert-warning">
-                            {{ questions.length }}
-                            <strong>Sorry</strong> There are no questions available.
-                        </div>
-
-                        <!-- pagination goes here -->
-                    </div>
+                    <questions></questions>
+                    
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-import QuestionExcerpt from '../components/QuestionExcerpt.vue';
+import Questions from '../components/Questions.vue';
 
 export default {
 
     components: {
-        QuestionExcerpt
-    },
-
-    data () {
-        return {
-            questions: []
-        }    
-    },
-
-    mounted () {
-        this.fetchQuestions();
-    },
-
-    methods: {
-        fetchQuestions () {
-            axios.get('/questions')
-                .then(( {data} ) => {
-                    this.questions = data.data
-                });
-        }
+        Questions
     }
+    
 }
 
 </script>
