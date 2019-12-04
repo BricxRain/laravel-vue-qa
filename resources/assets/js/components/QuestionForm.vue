eb73357b31f3ad3f70b4493682be9ae124a2a258
Bricx Carasco <bricxcarasco.gss@gmail.com>
2019-12-04 11:13:05 +0800

    Create edit question component and functions

--- resources/assets/js/components/QuestionForm.vue
+++ resources/assets/js/components/QuestionForm.vue
@@ -31,6 +31,13 @@ import MEditor from './MEditor.vue';
 
 export default {
     
+    props: {
+        isEdit: {
+            type: Boolean,
+            define: false
+        }
+    },
+
     components: {
         MEditor
     },
@@ -48,11 +55,15 @@ export default {
 
     mounted () {
         EventBus.$on('error', errors => this.errors = errors);
+
+        if (this.isEdit) {
+            this.fetchQuestion();
+        }
     },
 
     computed: {
         buttonText () {
-            return 'Ask Question';
+            return this.isEdit ? 'Update Question' : 'Ask Question';
         }
     },
 
@@ -69,6 +80,17 @@ export default {
                 'form-control',
                 this.errors[column] && this.errors[column][0] ? 'is-invalid' : ''
             ]
+        },
+
+        fetchQuestion () {
+            axios.get('/questions/' + this.$route.params.id)
+                .then(({ data }) => {
+                    this.title = data.title;
+                    this.body = data.body;
+                })
+                .catch(error => {
+                    console.log(error.response);
+                });
         }
     }
 
